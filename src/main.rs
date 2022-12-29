7d423f1fcc086ba701cecefb35cb0941e447aa81
Satoshi Tanaka <overthere555@gmail.com>
2022-12-30 07:59:23 +0900

    Implement control but unsuccessful

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use embedded_hal::delay::DelayUs;
+use std::time::Duration;
+use complementary_filter::ComplemtaryFilter;
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use esp_idf_hal::{prelude::*, spi::{SpiDeviceDriver, Dma, SpiConfig}, gpio::{Gpio0, PinDriver}};
+use esp_idf_hal::{prelude::*, spi::{SpiDeviceDriver, Dma, SpiConfig}, gpio::{Gpio0, PinDriver}, i2c::{I2cDriver, self}};
 use esp_idf_svc::timer::EspTimerService;
+use mpu6050::Mpu6886;
+use nalgebra::{vector, matrix};
 use shift_stepper::{ShiftStepper, SpeedController};
 
 mod shift_stepper;
+mod complementary_filter;
+
+const CONTROL_PERIOD: Duration = Duration::from_millis(10);
+const STEPS_PER_ROTATION: f32 = 240.0;
+const MAX_MOTOR_ANG_VEL: f32 = 4.0 * core::f32::consts::PI;
 
 fn main() {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
@@ -16,6 +24,18 @@ fn main() {
     
     let button = PinDriver::input(peripherals.pins.gpio39).unwrap();
 
+    // Initialize I2C for IMU
+    let i2c = I2cDriver::new(
+        peripherals.i2c0,
+        peripherals.pins.gpio25, peripherals.pins.gpio21,
+        &i2c::config::Config::new().baudrate(400.kHz().into()).sda_enable_pullup(true).scl_enable_pullup(true)
+    ).unwrap();
+
+    // Initialize IMU
+    let mut imu = Mpu6886::new(i2c);
+    imu.init(&mut delay).unwrap();
+
+    // Initialize SPI for stepper driver
     let spi = SpiDeviceDriver::new_single(
         peripherals.spi2,
         peripherals.pins.gpio19, peripherals.pins.gpio22, Option::<Gpio0>::None,
@@ -23,18 +43,30 @@ fn main() {
         &SpiConfig::new().baudrate(100.kHz().into()).data_mode(embedded_hal::spi::MODE_0)
     ).unwrap();
 
+    // Initialize stepper driver
     let mut steppers = ShiftStepper::new(spi);
     steppers.init().unwrap();
     steppers.set_active(false, false).unwrap();
 
+    // Initialize speed control for steppers
     let mut timer_service = EspTimerService::new().unwrap();
-
     let mut speed_controller = SpeedController::new(
         steppers, &mut timer_service, 0.001
     ).unwrap();
 
+    // Initialize complementary filter
+    let mut filter = ComplemtaryFilter::new(CONTROL_PERIOD.as_secs_f32());
+
+    // For button handling
     let mut active = false;
     let mut prev_pressed = false;
+
+    // For control
+    let dt = CONTROL_PERIOD.as_secs_f32();
+    let mut motor_ang_vel = 0.0;    // radians per sec
+    let control_gain = matrix![-97.42137788, -20.48627806, -3.16227766];    // LQR
+
+    let mut last_time = std::time::Instant::now();
 
     loop {
         let pressed = button.is_low();
@@ -45,7 +77,21 @@ fn main() {
         }
         prev_pressed = pressed;
 
-        speed_controller.set_speed(120.0, 0.0);
-        delay.delay_ms(10).unwrap();
+        let accel = imu.get_acc().unwrap();
+        let gyro = imu.get_gyro().unwrap();
+        let accel_angle = -accel.y.atan2(-accel.z);   // Becomes 0 when a display is facing up
+        let ang_vel = gyro.x;
+        let angle = filter.filter(accel_angle, ang_vel);
+
+        let state = vector![angle, ang_vel, motor_ang_vel];
+
+        let motor_accel = (-control_gain * state)[(0, 0)];
+        motor_ang_vel = (motor_ang_vel + motor_accel * dt).clamp(-MAX_MOTOR_ANG_VEL, MAX_MOTOR_ANG_VEL);
+
+        let steps_per_sec = STEPS_PER_ROTATION * motor_ang_vel / (2.0 * core::f32::consts::PI);
+        speed_controller.set_speed(-steps_per_sec, steps_per_sec);
+
+        std::thread::sleep(CONTROL_PERIOD - last_time.elapsed());
+        last_time = std::time::Instant::now();
     }
 }
