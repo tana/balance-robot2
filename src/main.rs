0c739601b6777e7e516cc140b164f26e214cf858
Satoshi Tanaka <overthere555@gmail.com>
2022-12-29 11:39:00 +0900

    Constant speed pulse generation for steppers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use embedded_hal::delay::DelayUs;
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use esp_idf_hal::{prelude::*, spi::{SpiDeviceDriver, Dma, SpiConfig}, gpio::Gpio0, delay};
-use shift_stepper::ShiftStepper;
+use esp_idf_hal::{prelude::*, spi::{SpiDeviceDriver, Dma, SpiConfig}, gpio::Gpio0};
+use esp_idf_svc::timer::EspTimerService;
+use shift_stepper::{ShiftStepper, SpeedController};
 
 mod shift_stepper;
 
@@ -10,32 +12,28 @@ fn main() {
     esp_idf_sys::link_patches();
 
     let peripherals = Peripherals::take().unwrap();
+    let mut delay = esp_idf_hal::delay::FreeRtos;
 
     let spi = SpiDeviceDriver::new_single(
         peripherals.spi2,
         peripherals.pins.gpio19, peripherals.pins.gpio22, Option::<Gpio0>::None,
         Dma::Disabled, Some(peripherals.pins.gpio23),
-        &SpiConfig::new().baudrate(KiloHertz(100).into()).data_mode(embedded_hal::spi::MODE_0)
+        &SpiConfig::new().baudrate(100.kHz().into()).data_mode(embedded_hal::spi::MODE_0)
     ).unwrap();
 
     let mut steppers = ShiftStepper::new(spi);
     steppers.init().unwrap();
 
+    let mut timer_service = EspTimerService::new().unwrap();
+
+    let mut speed_controller = SpeedController::new(
+        steppers, &mut timer_service, 0.001
+    ).unwrap();
+
     loop {
-        for _ in 0..240 {
-            steppers.step_motor1(true).unwrap();
-            steppers.step_motor2(false).unwrap();
-            delay::FreeRtos::delay_ms(10);
-        }
-
-        delay::FreeRtos::delay_ms(1000);
-
-        for _ in 0..240 {
-            steppers.step_motor1(false).unwrap();
-            steppers.step_motor2(true).unwrap();
-            delay::FreeRtos::delay_ms(10);
-        }
-
-        delay::FreeRtos::delay_ms(1000);
+        speed_controller.set_speed(120.0, 0.0);
+        delay.delay_ms(1000).unwrap();
+        speed_controller.set_speed(-120.0, 0.0);
+        delay.delay_ms(1000).unwrap();
     }
 }
--- src/shift_stepper.rs
+++ src/shift_stepper.rs
@@ -1,4 +1,14 @@
+use std::sync::{Mutex, Arc};
 use embedded_hal::spi::{SpiDevice, SpiBusWrite};
+use embedded_svc::timer::{TimerService, PeriodicTimer};
+
+#[derive(Clone, Default, PartialEq, Eq, Debug)]
+pub enum Direction {
+    #[default]
+    Still,
+    Forward,
+    Backward
+}
 
 // Half-step sequence
 // Due to a mistake of PCB design, a weird bit order is used.
@@ -40,14 +50,17 @@ where
         self.send()
     }
 
-    pub fn step_motor1(&mut self, forward: bool) -> Result<(), SPI::Error> {
-        self.motor1_phase = update_phase(self.motor1_phase, forward);
-
-        self.send()
-    }
-
-    pub fn step_motor2(&mut self, forward: bool) -> Result<(), SPI::Error> {
-        self.motor2_phase = update_phase(self.motor2_phase, forward);
+    pub fn step(&mut self, motor1_dir: Direction, motor2_dir: Direction) -> Result<(), SPI::Error> {
+        self.motor1_phase = match motor1_dir {
+            Direction::Still => self.motor1_phase,
+            Direction::Forward => update_phase(self.motor1_phase, true),
+            Direction::Backward => update_phase(self.motor1_phase, false)
+        };
+        self.motor2_phase = match motor2_dir {
+            Direction::Still => self.motor2_phase,
+            Direction::Forward => update_phase(self.motor2_phase, true),
+            Direction::Backward => update_phase(self.motor2_phase, false)
+        };
 
         self.send()
     }
@@ -68,4 +81,90 @@ fn update_phase(phase: usize, forward: bool) -> usize {
     };
 
     next.rem_euclid(SEQUENCE.len() as isize) as usize
+}
+
+pub struct SpeedController<S, T> where
+    S: SpiDevice,
+    S::Bus: SpiBusWrite,
+    T: TimerService
+{
+    #[allow(dead_code)]
+    steppers: Arc<Mutex<ShiftStepper<S>>>,
+    #[allow(dead_code)]
+    timer: T::Timer,
+    increment: Arc<Mutex<(i32, i32)>>,
+    timer_period: f32
+}
+
+const COUNTER_MAX: i32 = 1000;
+
+impl<S, T> SpeedController<S, T> where
+    S: SpiDevice + Send + 'static,
+    S::Bus: SpiBusWrite,
+    T: TimerService
+{
+    pub fn new(steppers: ShiftStepper<S>, timer_serice: &mut T, timer_period: f32) -> Result<Self, T::Error> {
+        let steppers = Arc::new(Mutex::new(steppers));
+        let increment = Arc::new(Mutex::new((0i32, 0i32)));
+
+        let mut timer = {
+            let steppers = Arc::clone(&steppers);
+            let increment = Arc::clone(&increment);
+
+            let mut motor1_counter = 0;
+            let mut motor2_counter = 0;
+
+            timer_serice.timer(move || {
+                let (motor1_increment, motor2_increment) = {
+                    let increment = increment.lock().unwrap();
+                    *increment
+                };
+
+                let motor1_dir = if motor1_counter + motor1_increment >= COUNTER_MAX {
+                    motor1_counter = motor1_counter + motor1_increment - COUNTER_MAX;
+                    Direction::Forward
+                } else if motor1_counter + motor1_increment < 0 {
+                    motor1_counter = motor1_counter + motor1_increment + COUNTER_MAX;
+                    Direction::Backward
+                } else {
+                    motor1_counter += motor1_increment;
+                    Direction::Still
+                };
+
+                let motor2_dir = if motor2_counter + motor2_increment >= COUNTER_MAX {
+                    motor2_counter = motor2_counter + motor2_increment - COUNTER_MAX;
+                    Direction::Forward
+                } else if motor2_counter + motor2_increment < 0 {
+                    motor2_counter = motor2_counter + motor2_increment + COUNTER_MAX;
+                    Direction::Backward
+                } else {
+                    motor2_counter += motor2_increment;
+                    Direction::Still
+                };
+
+                {
+                    let mut steppers = steppers.lock().unwrap();
+                    steppers.step(motor1_dir, motor2_dir).unwrap();
+                }
+            })?
+        };
+
+        timer.every(core::time::Duration::from_secs_f32(timer_period))?;
+
+        Ok(Self {
+            steppers,
+            timer,
+            increment,
+            timer_period
+        })
+    }
+
+    pub fn set_speed(&mut self, motor1_steps_per_sec: f32, motor2_steps_per_sec: f32) {
+        let mut increment = self.increment.lock().unwrap();
+        *increment = (self.speed_to_increment(motor1_steps_per_sec), self.speed_to_increment(motor2_steps_per_sec));
+    }
+
+    fn speed_to_increment(&self, steps_per_sec: f32) -> i32 {
+        (steps_per_sec * self.timer_period * (COUNTER_MAX as f32)).round() as i32
+    }
 }
